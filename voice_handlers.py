953b024eae86b1f48a58d4e548f6092dd41cbf13
danielgwilson <daniel@danielgwilson.com>
2016-02-14 03:09:23 -0800

    Working version, beatboxes

--- voice_handlers.py
+++ voice_handlers.py
@@ -27,52 +27,9 @@ def launch_request_handler(request):
 def session_ended_request_handler(request):
     return r.create_response(message="Goodbye!")
 
-<<<<<<< HEAD
-
-@VoiceHandler(intent='StartLike')
-def get_rapper_intent_handler(request):
-    """
-    Use the 'intent' field in the VoiceHandler to map to the respective intent.
-    You can insert arbitrary business logic code here
-    """
-
-    chains = {}
-    for file in os.listdir("models"):
-        chains[file[:-5]] = get_model("models/%s" % file)
-
-    # Get variables like userId, slots, intent name etc from the 'Request' object
-    rapper = request.get_slot_value("Rapper")
-    rapper = rapper if rapper else ""
-
-    with open("models/intros.json") as file:
-        intros = json.load(file)
-
-    try:
-        intro = intros[rapper]
-    except KeyError:
-        intro = ""
-
-    rap = get_rhyme(chains[rapper], 8)
-
-    #Use ResponseBuilder object to build responses and UI cards
-    card = r.create_card(title="Rapping",
-                         subtitle=None,
-                         content=('<speak>Aight yo I\'m gonna rap. Alexa. <break time="1.5s" /> Start rapping. <break time="1.5s" /> drop me a fat beat.' + rap + '<audio src="https://s3.amazonaws.com/danielgwilson.com/MLG+Horns+Sound+Effect.mp3" /> </speak>')
-
-
-    return r.create_response(message=('<speak>Aight yo I\'m gonna rap. Alexa. <break time="1.5s" /> Start rapping. <break time="1.5s" /> drop me a fat beat.' + rap + '<audio src="https://s3.amazonaws.com/danielgwilson.com/MLG+Horns+Sound+Effect.mp3" /> </speak>'),
-                             end_session=False,
-                             card_obj=card)
-
-
-@VoiceHandler(intent="Start")
+@VoiceHandler(intent="DropBeat")
 def call_back_intent_handler(request):
     """
     You can insert arbitrary business logic code here
     """
-    chains = {}
-    for file in os.listdir("models"):
-        chains[file[:-5]] = get_model("models/%s" % file)
-
-    rap = get_rhyme(chains["toponehundredraps"], 8)
-    return r.create_response(message='<speak>Aight yo I\'m gonna rap. Alexa. <break time="1.5s" /> Start rapping. <break time="1.5s" /> drop me a fat beat.' + rap + '<audio src="https://s3.amazonaws.com/danielgwilson.com/MLG+Horns+Sound+Effect.mp3" /> </speak>')
+    return r.create_response(message="boots and cats and boots and cats and boots and cats and boots and cats and boots and cats and boots and cats and boots and cats and boots and cats and boots and cats and boots and cats and boots and cats and boots and cats and boots and cats and boots and cats and boots and cats and boots and cats and boots and cats and ")
