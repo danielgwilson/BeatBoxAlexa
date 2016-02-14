8920859c6903f14341d4eb36fa513b615fa2a743
danielgwilson <daniel@danielgwilson.com>
2016-02-14 01:49:05 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# RapBattleAlexa
+![Build](https://img.shields.io/badge/build-passing-brightgreen.svg "Build")
+![License](https://img.shields.io/badge/license-MIT-blue.svg "License")
+
+Alexa can spit bars now. Check it out! Developed at TreeHacks 2016!
+
+![Rappers](http://ppcorn.com/us/wp-content/uploads/sites/14/2015/04/The-Top-10-Greatest-Rappers-of-All-Time-PPCORN.jpg "Rappers")
+
+## Credits
+- [ask-alexa-pykit](https://github.com/anjishnu/ask-alexa-pykit)
+- [mc-andrey](https://github.com/shazeline/mc-andrey)
+
+## Dependencies
+- [beautifulsoup4](https://pypi.python.org/pypi/beautifulsoup4)
+- [cmudict](http://www.speech.cs.cmu.edu/cgi-bin/cmudict)
+- [nltk](http://www.nltk.org/)
+- [requests](http://docs.python-requests.org/en/master/)
+
+## Todos:
+- Record user, include in Markov -> Literals for audio retrieval
+- Rhythm
+- Combine
+- Get working on Alexa
---
+++ voice_handlers.py
@@ -0,0 +1,78 @@
+from alexa.ask.utils import VoiceHandler, ResponseBuilder as r
+
+"""
+In this file we specify default event handlers which are then populated into the handler map using metaprogramming
+Copyright Anjishnu Kumar 2015
+
+Each VoiceHandler function receives a ResponseBuilder object as input and outputs a Response object
+A response object is defined as the output of ResponseBuilder.create_response()
+"""
+
+def default_handler(request):
+    """ The default handler gets invoked if no handler is set for a request """
+    return r.create_response(message="Just ask")
+
+
+@VoiceHandler(request_type="LaunchRequest")
+def launch_request_handler(request):
+    """
+    Annoatate functions with @VoiceHandler so that they can be automatically mapped
+    to request types.
+    Use the 'request_type' field to map them to non-intent requests
+    """
+    return r.create_response(message="Hello Welcome to My Recipes!")
+
+
+@VoiceHandler(request_type="SessionEndedRequest")
+def session_ended_request_handler(request):
+    return r.create_response(message="Goodbye!")
+
+<<<<<<< HEAD
+
+@VoiceHandler(intent='StartLike')
+def get_rapper_intent_handler(request):
+    """
+    Use the 'intent' field in the VoiceHandler to map to the respective intent.
+    You can insert arbitrary business logic code here
+    """
+
+    chains = {}
+    for file in os.listdir("models"):
+        chains[file[:-5]] = get_model("models/%s" % file)
+
+    # Get variables like userId, slots, intent name etc from the 'Request' object
+    rapper = request.get_slot_value("Rapper")
+    rapper = rapper if rapper else ""
+
+    with open("models/intros.json") as file:
+        intros = json.load(file)
+
+    try:
+        intro = intros[rapper]
+    except KeyError:
+        intro = ""
+
+    rap = get_rhyme(chains[rapper], 8)
+
+    #Use ResponseBuilder object to build responses and UI cards
+    card = r.create_card(title="Rapping",
+                         subtitle=None,
+                         content=('<speak>Aight yo I\'m gonna rap. Alexa. <break time="1.5s" /> Start rapping. <break time="1.5s" /> drop me a fat beat.' + rap + '<audio src="https://s3.amazonaws.com/danielgwilson.com/MLG+Horns+Sound+Effect.mp3" /> </speak>')
+
+
+    return r.create_response(message=('<speak>Aight yo I\'m gonna rap. Alexa. <break time="1.5s" /> Start rapping. <break time="1.5s" /> drop me a fat beat.' + rap + '<audio src="https://s3.amazonaws.com/danielgwilson.com/MLG+Horns+Sound+Effect.mp3" /> </speak>'),
+                             end_session=False,
+                             card_obj=card)
+
+
+@VoiceHandler(intent="Start")
+def call_back_intent_handler(request):
+    """
+    You can insert arbitrary business logic code here
+    """
+    chains = {}
+    for file in os.listdir("models"):
+        chains[file[:-5]] = get_model("models/%s" % file)
+
+    rap = get_rhyme(chains["toponehundredraps"], 8)
+    return r.create_response(message='<speak>Aight yo I\'m gonna rap. Alexa. <break time="1.5s" /> Start rapping. <break time="1.5s" /> drop me a fat beat.' + rap + '<audio src="https://s3.amazonaws.com/danielgwilson.com/MLG+Horns+Sound+Effect.mp3" /> </speak>')
